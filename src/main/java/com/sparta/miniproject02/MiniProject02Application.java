ce9d8f72b447a1291f56bca2fd44137c05777749
GYUMIN KWON <rbals7398@naver.com>
2022-04-08 16:06:26 +0900

    firstCommit

---
+++ src/main/java/com/sparta/miniproject02/MiniProject02Application.java
@@ -0,0 +1,13 @@
+package com.sparta.miniproject02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProject02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProject02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/miniproject02/MiniProject02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.miniproject02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProject02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
